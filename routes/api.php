4f4450d14e87bc94e5ea41034f370079e29f6263
Muhammad Awan <muhammad.awan@protonmail.com>
2024-07-29 22:00:28 +1000

    add products controller for restful actions

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
 
+Route::get('/Products', [ProductsController::class, 'index']);
+Route::get('/Products/{id}', [ProductsController::class, 'show']);
+Route::post('/Products', [ProductsController::class, 'store']);
+Route::put('/Products/{id}', [ProductsController::class, 'update']);
+Route::delete('/Products/{id}', [ProductsController::class, 'destroy']);
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
